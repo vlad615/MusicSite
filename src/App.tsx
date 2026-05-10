c1e1316f62655f5ffcd09ecade653fbe4a9398f9
VladislavAmbrosov <ambrosov413@gmail.com>
2026-05-10 14:15:29 +0300

    refactor header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import './App.scss'
 import { Header } from './layout/header/Header'
+import type { MenuProps } from './componentes/menu/Menu'
 
 export function App() {
+  const menuItems: MenuProps = {menu: [
+    {id: "main", title: "Home" }, 
+    {id: "episodes", title: "Episodes"}, 
+    {id: "about", title: "About"}, 
+    {id: "contact", title: "Contact"}]}
+  
 
   return (
     <>
-      <Header />
+      <Header {...menuItems}/>
     </>
   )
 }
--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,11 +1,23 @@
+import { useEffect, useState } from "react";
+import { Icon } from "../../componentes/icons/Icon";
 import s from "./Header.module.scss";
+import { Menu, type MenuProps } from "../../componentes/menu/Menu";
+import { MobileMenu } from "./mobileMenu/MobileMenu";
 
-export const Header = () => {
+export const Header = (props: MenuProps) => {
+    const [width, setWidth] = useState(window.innerWidth)
+
+    useEffect(()=>{
+        window.addEventListener("resize", () => setWidth(window.innerWidth))
+    }, [])
+
     return(
         <header>
             <div className="container">
                 <div className={s.wrapper}>
+                    <Icon icon="logo" width={166} height={33} boxview="0,0,166,33"/>
 
+                    {width > 768? <Menu {...props}/> : <MobileMenu {...props}/>}
                 </div>
             </div>
         </header>
