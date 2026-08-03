70c10d743383203dc4088fdd8da6a0fe80a8aba7
VladislavAmbrosov <ambrosov413@gmail.com>
2026-08-03 11:50:53 +0300

    ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/MusicSite/",
   plugins: [react()],
   css: {
-        modules: {
-        localsConvention: 'dashesOnly', // или 'camelCaseOnly', 'dashes', 'camelCase'
-        generateScopedName: '[name]__[local]___[hash:base64:5]', // формат: ComponentName_className_hash
-        },
+    modules: {
+      localsConvention: "dashesOnly", // или 'camelCaseOnly', 'dashes', 'camelCase'
+      generateScopedName: "[name]__[local]___[hash:base64:5]", // формат: ComponentName_className_hash
     },
-})
+  },
+});
